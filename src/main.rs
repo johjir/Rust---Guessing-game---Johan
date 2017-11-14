eccb4d192661be86ddd4f6dc5a5c3af30992c335
nannap <33249020+nannap@users.noreply.github.com>
2017-11-14 16:17:26 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,32 +4,44 @@ use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
 
+// Function requesting user input and returning a u32 or an error string
+fn userinput() -> Result<u32, String>{
+
+    let mut guess = String::new();
+        
+    io::stdin().read_line(&mut guess)           // Requests user input, stored in guess
+        .expect("Failed to read line");
+            
+    let guess: u32 = match guess.trim().parse() {       // Attempts to parse guess to a u32
+        Ok(num) => num,
+        Err(_)  => return Err(String::from("your guess needs to be a number")), // Returns an error str otherwise 
+    };
+    Ok(guess)
+
+}
+
 fn main() {
     println!("Guess the number!");
     
     let secret_number = rand::thread_rng().gen_range(1,101);
     
     let mut guess_tracker = 0;
+    let mut guess: u32;
     
     //println!("The secret number is {}", secret_number);
     
     loop {
         println!("Please input your guess.");
         
-        let mut guess = String::new();
-        
-        io::stdin().read_line(&mut guess)
-            .expect("Failed to read line");
-            
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_)  => {
-                println!("Your guess needs to be a number!");
+        match userinput() {
+            Ok(g) => guess = g,
+            Err(err) => {
+                println!("in parsing u32, {}", err);
                 continue;
             }
         };
         
-        guess_tracker = guess_tracker + 1;
+        guess_tracker += 1;
         
         println!("You guessed: {}", guess);
         
