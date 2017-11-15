bc5e8c9d8d7973bd975190c71faacb7542b656fc
nannap <33249020+nannap@users.noreply.github.com>
2017-11-15 19:13:26 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,9 @@ fn main() {
     
     let secret_number = rand::thread_rng().gen_range(1,101);
     
-    let mut guess_tracker = 0;
+    let mut guess_tracker: u32 = 0;
+    
+    let mut guessvec: Vec<(u32, String)> = Vec::new();          // A vector for the guesses is created
     
     //println!("The secret number is {}", secret_number);
     
@@ -21,7 +23,7 @@ fn main() {
         io::stdin().read_line(&mut guess)
             .expect("Failed to read line");
             
-        let guess: u32 = match guess.trim().parse() {
+        let guessnum: u32 = match guess.clone().trim().parse() {
             Ok(num) => num,
             Err(_)  => {
                 println!("Your guess needs to be a number!");
@@ -31,13 +33,21 @@ fn main() {
         
         guess_tracker = guess_tracker + 1;
         
-        println!("You guessed: {}", guess);
+        guessvec.push((guess_tracker, guess));          // Current guess and its number is pushed onto the vector
         
-        match guess.cmp(&secret_number) {
+        println!("You guessed: {}", guessnum);
+        
+        match guessnum.cmp(&secret_number) {
             Ordering::Less      => println!("Too small!"),
             Ordering::Greater   => println!("Too big!"),
             Ordering::Equal     => {
                 println!("You win!");
+                for guesselement in guessvec.clone() {
+                    println!("Guess {} was {}", guesselement.0, guesselement.1);    // All guesses are printed out (but with \n at the end of all strings as a side effect of storing unaltered user input)
+                }
+                for guesselement in guessvec {
+                    println!("Guess {} was {}", guesselement.0, guesselement.1);    // The history is printed again, after the first was printed from a clone
+                }
                 println!("Total number of guesses was {}", guess_tracker);
                 break;
             }    
