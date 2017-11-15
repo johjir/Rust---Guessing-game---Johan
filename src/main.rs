ceeb29fa97801c65b426a1afed81884c299c139d
nannap <33249020+nannap@users.noreply.github.com>
2017-11-15 19:19:33 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -33,6 +33,7 @@ fn main() {
         
         guess_tracker = guess_tracker + 1;
         
+        guess.pop();
         guessvec.push((guess_tracker, guess));          // Current guess and its number is pushed onto the vector
         
         println!("You guessed: {}", guessnum);
@@ -43,7 +44,7 @@ fn main() {
             Ordering::Equal     => {
                 println!("You win!");
                 for guesselement in guessvec.clone() {
-                    println!("Guess {} was {}", guesselement.0, guesselement.1);    // All guesses are printed out (but with \n at the end of all strings as a side effect of storing unaltered user input)
+                    println!("Guess {} was {}", guesselement.0, guesselement.1);    // All guesses are printed out 
                 }
                 for guesselement in guessvec {
                     println!("Guess {} was {}", guesselement.0, guesselement.1);    // The history is printed again, after the first was printed from a clone
